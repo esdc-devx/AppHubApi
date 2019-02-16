63270f1bc34398c5e3a499b5217aecca09c070d7
Calvin Rodo <calvinv@gmail.com>
2019-02-15 22:24:31 -0500

    Initial commit for apollo server (#1)
    * [WIP] initialized npm
    
    * [WIP] Initial server creation
    
    Added apollo-server and graphql to project
    Added initial dashboard code coverage project.
    
    * [WIP] Adding Dockerfile for dev environment
    
    * [WIP] adding proper license to package.json

---
+++ buildcontainer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build . -t apphubapi:latest
---
+++ index.js
@@ -0,0 +1,70 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const Projects = [
+  {
+    name: 'Foo',
+    coverage: 100,
+    lastUpdated: 'yyyy-MM-dd'
+  },
+  {
+    name: 'Bar',
+    coverage: 75,
+    lastUpdated: 'yyyy-MM-dd'
+  },
+  {
+    name: 'Baz',
+    coverage: 50,
+    lastUpdated: 'yyyy-MM-dd'
+  },
+  {
+    name: 'Far',
+    coverage: 25,
+    lastUpdated: 'yyyy-MM-dd'
+  },
+  {
+    name: 'Boo',
+    coverage: 0,
+    lastUpdated: 'yyyy-MM-dd'
+  }
+];
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  # This "Book" type can be used in other type declarations.
+  type Project {
+    name: String
+    coverage: Int
+    lastUpdated: String
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    projects: [Project]
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    projects: () => Projects,
+  },
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
